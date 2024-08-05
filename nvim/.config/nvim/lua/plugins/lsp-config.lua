5ad280e1362e185219174a4ba07972013bceb7a1
Courtney Bodett <courtney.bodett@gmail.com>
2024-08-05 14:50:48 -0700

    added new neovim plugins

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return {
+    {
+       "williamboman/mason.nvim",
+       config = function()
+          require("mason").setup()
+       end,
+    },
+    {
+       "williamboman/mason-lspconfig.nvim",
+          lazy = false,
+          opts = {
+            auto_install = true,
+          },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function ()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.powershell_es.setup({
+                capabilities = capabilities,
+                bundle_path = '/home/grislobo/.local'
+            })
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+        end,
+    },
+}
