47f05463eadecdd72f17dcb9a48670ad34ce3e93
Courtney Bodett <courtney.bodett@gmail.com>
2025-02-25 20:40:58 -0800

    trying out lualine
    set up lualine with nord theme

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    lazy = false,
+    config = function()
+        require('lualine').setup({
+            options = {
+                theme = 'nord',
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'filename'},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {'location'}
+            },
+        })
+    end,
+}
