305df033eea3a51414f6b0193e47e732a35e7e39
Courtney Bodett <courtney.bodett@gmail.com>
2025-02-28 15:11:49 -0800

    updated config
    updated lualine and noice config for visuals. added better code folding
    behavior

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require('lualine').setup({
             options = {
-                theme = 'nord',
+                theme = 'onenord',
             },
             sections = {
                 lualine_a = {'mode'},
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -12,6 +12,19 @@ vim.opt.swapfile = false
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- Configure code folding
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    callback = function()
+        if require("nvim-treesitter.parsers").has_parser() then
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+        else
+            vim.opt.foldmethod = "syntax"
+        end
+    end,
+})
+vim.opt.foldenable = false
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
