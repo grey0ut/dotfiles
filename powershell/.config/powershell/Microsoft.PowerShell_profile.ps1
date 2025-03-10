d209354b73d6e1eb9e7d9d8014f92802a91207f7
Courtney Bodett <courtney.bodett@gmail.com>
2025-03-10 14:34:53 -0700

    adjusted psreadline colors to better work with Nord theme

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -8,4 +8,9 @@ Invoke-Expression (&starship init powershell)
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 Remove-PSReadLineKeyHandler -Chord Ctrl+a
 Set-PSReadLineKeyHandler -Chord Ctrl+a -Function SelectAll
+$Colors = @{
+    Operator = "White"
+    Parameter = "Green"
+}
+Set-PSReadlineOption -Colors $Colors
 $psstyle.fileinfo.Directory = "`e[1m"
