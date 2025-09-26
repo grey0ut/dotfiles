961472c3f5255147a91d4fa54dd1908c09efde19
Courtney Bodett <courtney.bodett@gmail.com>
2025-09-26 16:03:07 -0700

    updating lsp
    upcoming breaking change in nvim-lspconfig:
    https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#important-%EF%B8%8F
    updated config to use appropriate language

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,44 +1,58 @@
 return {
     {
-       "williamboman/mason.nvim",
-       config = function()
-          require("mason").setup()
-       end,
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
     },
+
     {
-       "williamboman/mason-lspconfig.nvim",
-          lazy = false,
-          automatic_installation = true,
-          --opts = {
-          --  auto_install = true,
-          --},
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            -- install these automatically so binaries exist
+            ensure_installed = { "lua_ls", "pyright", "powershell_es" },
+            automatic_installation = true,
+        },
     },
+
     {
         "neovim/nvim-lspconfig",
         lazy = false,
-        config = function ()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
+            -- tiny helper so we don’t repeat ourselves
+            local function cfg(server, opts)
+                opts = opts or {}
+                opts.capabilities = capabilities
+                vim.lsp.config(server, opts)    -- define
+            end
+
+            -- LSP servers
+            cfg("lua_ls", {
+                settings = {
+                    Lua = { diagnostics = { globals = { "vim" } } },
+                },
             })
-            lspconfig.pyright.setup({
-                capabilities = capabilities
+
+            cfg("pyright", {})
+
+            cfg("powershell_es", {
+                bundle_path = vim.fn.expand("~/.config/powershell_es"),
+                init_options = { enableProfileLoading = false },
             })
-            lspconfig.powershell_es.setup({
-                capabilities = capabilities,
-                bundle_path = '~/.config/powershell_es',
-                init_options = {
-                    enableProfileLoading = false,
-                }
-            })
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+
+            -- now turn them on
+            vim.lsp.enable({ "lua_ls", "pyright", "powershell_es" })
+
+            -- keymaps
+            local map = vim.keymap.set
+            map("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+            map("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            map("n", "<leader>gr", vim.lsp.buf.references, { desc = "References" })
+            map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+            map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
         end,
     },
 }
