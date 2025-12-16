fa398a0751629a163f2131313da5bfc9f1f39bd5
Courtney Bodett <courtney.bodett@gmail.com>
2025-12-15 19:49:45 -0800

    moved treesitter to main branch
    breaking change in new installation of treesitter with existing config.
    modified to support the new main branch

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -13,17 +13,17 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Configure code folding
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    callback = function()
-        if require("nvim-treesitter.parsers").has_parser() then
-            vim.opt.foldmethod = "expr"
-            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-        else
-            vim.opt.foldmethod = "syntax"
-        end
-    end,
-})
-vim.opt.foldenable = false
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--     callback = function()
+--         if require("nvim-treesitter.parsers").has_parser() then
+--             vim.opt.foldmethod = "expr"
+--             vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+--         else
+--             vim.opt.foldmethod = "syntax"
+--         end
+--     end,
+-- })
+-- vim.opt.foldenable = false
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
