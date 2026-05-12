d244be46857e610519873fcbd9ea14ec3fcfd7c0
Courtney Bodett <courtney.bodett@gmail.com>
2026-05-12 16:23:46 -0700

    update script for new hyprctl syntax

--- hyprconf_laptop/.config/hypr/conf/refreshtoggle.sh
+++ hyprconf_laptop/.config/hypr/conf/refreshtoggle.sh
@@ -4,7 +4,7 @@
 CURRENT=$(hyprctl monitors | grep "eDP-1" -A 1 | awk -F @ 'NR>1 {print $2}' | awk -F . '{print $1}')
 
 if [[ $CURRENT = 120 ]]; then
-    hyprctl keyword monitor eDP-1,2880x1920@60,auto,1.5
+    hyprctl dispatch  'hl.monitor({ output = "eDP-1", mode = "2880x1920@60", scale = "1.5"})'
 else 
-    hyprctl keyword monitor eDP-1,2880x1920@120,auto,1.5
+    hyprctl dispatch  'hl.monitor({ output = "eDP-1", mode = "2880x1920@120", scale = "1.5"})'
 fi
