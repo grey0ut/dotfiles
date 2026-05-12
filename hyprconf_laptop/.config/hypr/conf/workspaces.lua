0921d8591a8152632a657672a8119b90611db091
Courtney Bodett <courtney.bodett@gmail.com>
2026-05-12 15:20:10 -0700

    updated workspace rules for no_gaps, fixed nextcloud autostart

--- hyprconf_laptop/.config/hypr/conf/workspaces.lua
+++ hyprconf_laptop/.config/hypr/conf/workspaces.lua
@@ -2,7 +2,12 @@
 ---- WORKSPACES ----
 --------------------
 
-hl.workspace_rule({ workspace = "w[t1]", gaps_out = 0, gaps_in = 0, no_border = true})
+hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, border_size = 0 })
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, rounding = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, border_size = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, rounding = 0 })
 hl.workspace_rule({ workspace = "1", monitor = "eDP-1",  default = true, persistent = true, layout = "scrolling" })
 hl.workspace_rule({ workspace = "2", monitor = "eDP-1", default = true, persistent = true,})
 hl.workspace_rule({ workspace = "3", monitor = "eDP-1", default = true, persistent = true,})
