beff898b7d80ed9cfbd74595e6227fccc24722ec
Courtney Bodett <courtney.bodett@gmail.com>
2026-06-12 20:42:39 -0700

    removed comments and updated to use new is_laptop function

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,4 +1,9 @@
 ------------------
+--- FUNCTIONS ----
+------------------
+local func = require("conf.functions")
+
+------------------
 ---- MONITORS ----
 ------------------
 require("conf.monitor")
@@ -7,14 +12,6 @@ require("conf.monitor")
 ---- AUTOSTART ----
 -------------------
 require("conf.autostart")
--- hl.on("hyprland.start", function () 
---   hl.exec_cmd("swaync & hyprpaper")
---   hl.exec_cmd("~/.config/waybar/scripts/startwaybar.sh")
---   hl.exec_cmd("/usr/lib/pam_kwallet_init")
---   hl.exec_cmd("systemctl --user start plasma-polkit-agent")
---   hl.exec_cmd("nextcloud")
---   hl.exec_cmd("hypridle")
--- end)
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
@@ -175,12 +172,6 @@ hl.config({
 --- GESTURES ---
 ----------------
 require("conf.gestures")
--- hl.gesture({
---     fingers = 4,
---     direction = "horizontal",
---     action = "workspace"
--- })
-
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({
@@ -192,137 +183,15 @@ hl.device({
 ---------------------
 ---- KEYBINDINGS ----
 ---------------------
-require("conf.keybindings")
--- local mainMod = "SUPER" -- Sets "Windows" key as main modifier
--- 
--- hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(terminal))
--- local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
--- -- closeWindowBind:set_enabled(false)
--- hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
--- hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
--- hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
--- hl.bind(mainMod .. " + space", hl.dsp.exec_cmd(menu))
--- hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
--- -- hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
--- 
--- -- Move focus with mainMod + arrow keys
--- hl.bind(mainMod .. " + h",  hl.dsp.focus({ direction = "left" }))
--- hl.bind(mainMod .. " + l", hl.dsp.focus({ direction = "right" }))
--- hl.bind(mainMod .. " + k",    hl.dsp.focus({ direction = "up" }))
--- hl.bind(mainMod .. " + j",  hl.dsp.focus({ direction = "down" }))
--- 
--- -- Switch workspaces with mainMod + [0-9]
--- -- Move active window to a workspace with mainMod + SHIFT + [0-9]
--- for i = 1, 10 do
---     local key = i % 10 -- 10 maps to key 0
---     hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
---     hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
--- end
--- 
--- -- Example special workspace (scratchpad)
--- hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
--- hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
--- 
--- -- Scroll through existing workspaces with mainMod + scroll
--- hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
--- hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
--- 
--- -- Move/resize windows with mainMod + LMB/RMB and dragging
--- hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
--- hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
--- 
--- -- Laptop multimedia keys for volume and LCD brightness
--- hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
--- hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
--- hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
--- hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
--- hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
--- hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
--- 
--- -- Requires playerctl
--- hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
--- hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
--- hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
--- hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
--- 
--- -- Scrolling layout
--- hl.bind(mainMod .. "+ period", hl.dsp.layout("move +col"))
--- hl.bind(mainMod .. "+ comma", hl.dsp.layout("swapcol l"))
--- hl.bind(mainMod .. "+ slash", hl.dsp.layout("colresize +conf"))
 
+if func.is_laptop() then
+    require("conf.keybind_laptop")
+else
+    require("conf.keybind_desktop")
+end
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
 --------------------------------
 require("conf.windowrule")
 require("conf.workspaces")
--- local suppressMaximizeRule = hl.window_rule({
---     -- Ignore maximize requests from all apps. You'll probably like this.
---     name  = "suppress-maximize-events",
---     match = { class = ".*" },
--- 
---     suppress_event = "maximize",
--- })
--- -- suppressMaximizeRule:set_enabled(false)
--- 
--- hl.window_rule({
---     -- Fix some dragging issues with XWayland
---     name  = "fix-xwayland-drags",
---     match = {
---         class      = "^$",
---         title      = "^$",
---         xwayland   = true,
---         float      = true,
---         fullscreen = false,
---         pin        = false,
---     },
---     no_focus = true,
--- })
--- 
--- hl.window_rule({
---     name = "signal-float",
---     match = {
---         title = "^(Signal)",
---     },
---     float  = true,
--- })
--- 
--- hl.window_rule({
---     name = "Firefox-opac",
---     match = {
---         class = "firefox",
---     },
---     opacity = "1.0 override",
--- })
--- 
--- hl.window_rule({
---     name = "Plasma-windowed",
---     match = {
---         class = "org.kde.plasmawindowed",
---     },
---     float = true,
--- })
--- 
--- hl.window_rule({
---     name = "10-floats",
---     match = {
---         workspace = 10,
---     },
---     float = true,
--- })
--- -- Hyprland-run windowrule
--- hl.window_rule({
---     name  = "move-hyprland-run",
---     match = { class = "hyprland-run" },
--- 
---     move  = "20 monitor_h-120",
---     float = true,
--- })
-
--- WORKSPACES
--- hl.workspace_rule({ workspace = "1", monitor = "DP-3",  default = true, persistent = true, layout = "scrolling" })
--- hl.workspace_rule({ workspace = "2", monitor = "DP-3", default = true, persistent = true,})
--- hl.workspace_rule({ workspace = "3", monitor = "DP-3", default = true, persistent = true,})
--- hl.workspace_rule({ workspace = "4", monitor = "DP-3", default = true, persistent = true,})
--- hl.workspace_rule({ workspace = "9", monitor = "DP-2", default = true, persistent = true, gaps_in = 1, gaps_out = 1, no_rounding = true, })
--- hl.workspace_rule({ workspace = "10", monitor = "DP-2", default = true, persistent = true, gaps_in = 1, gaps_out = 1, no_rounding = true, })
